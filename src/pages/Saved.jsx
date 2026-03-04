3cfd5a7cd2c457315378ceec296964f8f8579586
Dev Patel <pateld0514@gmail.com>
2026-03-04 04:17:51 +0000

    File changes

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -175,7 +175,7 @@ export default function SavedPage() {
     }
   };
 
-  if (loadingSaved) {
+  if (!currentUser || loadingSaved || loadingVendors) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
         <Loader2 className="w-10 h-10 md:w-12 md:h-12 animate-spin text-black mb-4" />
