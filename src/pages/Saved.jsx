e428b18f4681d47d32a5f77b0895353e55acb9e0
Dev Patel <pateld0514@gmail.com>
2026-03-04 05:44:39 +0000

    File changes

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// file head ok
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
